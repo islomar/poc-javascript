eafccd96a66297c02dcb8999984dd6b5f47cdf95
islomar <islomar@gmail.com>
2015-04-05 21:01:38 +0200

    Exercise 2-28: speed test using Join instead of Concat

--- codeschool-javascript-best-practices/level2-perfomance/proto.js
+++ codeschool-javascript-best-practices/level2-perfomance/proto.js
@@ -2,7 +2,7 @@ var IP = ["Nipping Global Variable", "Sneaky Forin", "Bulging Blocking Script"],
 GH = ["Switch Blocks", "Pesky Gnat", "Aiedra"],
 inhabitants = [IP,GH];
 
-function populationGetter(popn){
+function populationGetterConcat(popn){
   var list = "";
   for(var i = 0, x = popn.length; i < x; i++){
     for(var j = 0; j < popn[i].length; j++){
@@ -12,5 +12,20 @@ function populationGetter(popn){
   return list;
 }
 
-populationGetter(inhabitants);
-new SpeedTest(populationGetter, inhabitants, 100000).startTest();
+populationGetterConcat(inhabitants);
+
+function populationGetterJoin(popn){
+  var list = [];
+  for(var i = 0, x = popn.length; i < x; i++){
+    list.push(popn[i].join(', '));
+  }
+  return list.join(', ');
+}
+
+populationGetterJoin(inhabitants);
+
+var concatTest = new SpeedTest(populationGetterConcat, inhabitants, 100000);
+concatTest.startTest();
+
+var joinTest = new SpeedTest(populationGetterJoin, inhabitants);
+joinTest.startTest();
