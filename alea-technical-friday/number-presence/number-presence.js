f7aa7eb737ad712ffc8bcd0474c696ac29e79bf2
Isidro Lopez <ijlopez@alea-soluciones.com>
2016-02-12 13:27:01 +0100

    Update number examples about NaN

--- alea-technical-friday/number-presence/number-presence.js
+++ alea-technical-friday/number-presence/number-presence.js
@@ -2,7 +2,9 @@ console.log("typeOf NaN is " + (typeof NaN));
 
 console.log("NaN === NaN is " + (NaN === NaN));
 
-console.log("isNaN('42') is " + (isNaN("42"))); //Turns out that this method is looking strictly for the JS value NaN itself. Fun.
+console.log("isNaN(0/0) = " + (isNaN(0/0)));
+console.log("isNaN('42') = " + (isNaN("42"))); //Turns out that this method is looking strictly for the JS value NaN itself. Fun.
+console.log("isNaN(9) = " + (isNaN(9)));
 
 console.log("isNaN(NaN) is " + (isNaN(NaN)));
 
