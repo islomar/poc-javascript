3b8999e4a7776c37b4d5e4bd227674ebd2d79261
Isidro Lopez <ijlopez@alea-soluciones.com>
2016-02-12 13:23:47 +0100

    Update global variables example with window.

--- alea-technical-friday/global-variables/variables2.js
+++ alea-technical-friday/global-variables/variables2.js
@@ -1,5 +1,10 @@
 var numberOfOlts = 5;
 
 console.log("numberOfUsers: " + numberOfUsers);
+console.log("window.numberOfUsers: " + window.numberOfUsers);
+
 console.log("foo: " + foo);
+console.log("window.foo: " + window.foo);
+
 console.log("bar: " + bar);
+console.log("window.bar: " + window.bar);
