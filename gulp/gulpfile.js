a1b5bc746d15496018049f02340b5dd9b64d79cb
Isidro Lopez <islomar@gmail.com>
2016-07-26 19:55:59 +0200

    Example with watch()

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -12,3 +12,8 @@ gulp.task('sass', function(){
 });
 
 gulp.task('default', ['hello', 'sass']);
+
+gulp.task('watch', ['hello', 'sass'], () => {
+  gulp.watch('app/scss/*.scss', ['scss']);  // Keep watching for any changes in SCSS files and rerun the css task when so
+  gulp.watch('app/js/*.js', ['es6']);       // Keep Watching for any changes in JS files and rerun the es6 task when so
+});
